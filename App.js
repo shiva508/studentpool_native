b9097915cc2cf034b0a2283830313516f3a10c29
shiva508 <dasarishiva1@gmail.com>
2022-11-09 08:22:22 +0530

    start game functionality

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 import BasicUi from "./components/BasicUi";
 import FluxBoxUi from "./components/FluxBoxUi";
+import GussNumber from "./components/GussNumber";
 
 export default function App() {
   return (
     <>
       <StatusBar style="dark" />
-      <BasicUi></BasicUi>
+      <GussNumber />
     </>
-  ); //<FluxBoxUi></FluxBoxUi>;
+  );
+  //<FluxBoxUi></FluxBoxUi>;
+  //<BasicUi></BasicUi>
 }
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import { Alert, StyleSheet, TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+const StartGameScreen = () => {
+  const [enteredNumber, setEnteredNumber] = useState("");
+  const numberInputHandler = (inputValue) => {
+    console.log(inputValue);
+    setEnteredNumber(inputValue);
+  };
+
+  const resetNumberHandler = () => {
+    setEnteredNumber("");
+  };
+  const confirmInputHandler = () => {
+    const chosenNumber = parseInt(enteredNumber);
+    console.log(chosenNumber);
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert("Invalid number", "Entered number is invalid", [
+        { text: "Okay", style: "destructive", onPress: resetNumberHandler },
+      ]);
+      return;
+    }
+  };
+
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="numeric"
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={enteredNumber}
+        onChangeText={numberInputHandler}
+      />
+      <View style={styles.buttonsOuterContainer}>
+        <View style={styles.buttonsInnerContainer}>
+          <PrimaryButton onPress={resetNumberHandler}>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonsInnerContainer}>
+          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+        </View>
+      </View>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    backgroundColor: "#3b021f",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "block",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    textAlign: "center",
+  },
+  buttonsOuterContainer: {
+    flexDirection: "row",
+  },
+  buttonsInnerContainer: {
+    flex: 1,
+  },
+});
+export default StartGameScreen;
